68d81e35cff1f42f3314242ac61740f65b9fb33b
Bhanumaan <101312783+Bhanumaan@users.noreply.github.com>
2022-03-10 11:21:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+a="a b c d"
+print(len(a))
+var=a.capitalize()
+print(var)
+
+b="BHANUMAAN"
+var=b.casefold()
+print(var)
+
+txt="I love apples , apple are my favorite fruit "
+x=txt.count("apple")
+print(x)
+x=txt.count("apple",10,24)
+print(x)
+
+txt="hello welcome to my world."
+x=txt.endswith(".")
+print(x)
